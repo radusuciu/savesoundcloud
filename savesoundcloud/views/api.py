0b72d4ffb3b30bac82b796639ab945faef3d3bf5
Radu Suciu <radusuciu@gmail.com>
2017-07-18 12:29:30 -0700

    Remove individual endpoint csv

--- savesoundcloud/views/api.py
+++ savesoundcloud/views/api.py
@@ -14,21 +14,6 @@ def get_endpoint(user, endpoint):
     return jsonify(api.consume(user, endpoint))
 
 
-@api_blueprint.route('/user/<user>/<endpoint>.csv')
-def get_csv(user, endpoint):
-    if endpoint not in api.ENDPOINTS:
-        abort(404)
-
-    filename, file = api.to_csv(user, endpoint)
-
-    return send_file(
-        file,
-        attachment_filename=filename,
-        as_attachment=True,
-        mimetype='text/csv'
-    )
-
-
 @api_blueprint.route('/find_user')
 def find_users():
     return jsonify(api.find_users(request.args.get('term')))
