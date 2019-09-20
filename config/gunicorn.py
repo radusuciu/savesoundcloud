ac0df2fcef7b5aaf0308a769e27771668faca40f
Radu Suciu <radusuciu@gmail.com>
2019-09-19 20:26:13 -0400

    Make compatible with server setup

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,8 +1,9 @@
 import multiprocessing
 
-bind = '0.0.0.0:5000'
+bind = '0.0.0.0:5001'
+worker_class = 'gevent'
 workers = multiprocessing.cpu_count() * 2 + 1
 loglevel = 'INFO'
 errorlog = '-'
 accesslog = '-'
-timeout = 120
+timeout = 180
